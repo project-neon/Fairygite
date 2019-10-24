3a80c8e346807639724e0a345d611231162948b2
Alexsandro Francisco <alexsandro2134@gmail.com>
2019-10-24 18:36:58 -0300

    Work on first try :D

--- NeonBattlerControllerServer/NeonBattlerControllerServer.ino
+++ NeonBattlerControllerServer/NeonBattlerControllerServer.ino
@@ -18,6 +18,8 @@
 // WEAPONS MOTOR PINS
 #define WEAPONPWM 15
 
+#define MESSAGE_TIMEOUT 50 //milli
+
 Servo weaponMotor;
 
 const char* ssid = "AndroidAP5554";
@@ -33,9 +35,12 @@ int pow_left = 0;
 int pow_right = 0;
 int pow_left_dir = 0;
 int pow_right_dir = 0;
+int pow_esc = 0;
 
+int last_message_time = 0;
 
 int weapon_status = 0;
+int weapon_power = 0;
 
 WiFiUDP udp;
 
@@ -46,15 +51,38 @@ void setup() {
   pinMode(ML2, OUTPUT);
   pinMode(MLPWM, OUTPUT);
   pinMode(MRPWM, OUTPUT);
-//  
-  // put your setup code here, to run once:
-  
   Serial.begin(115200);
   delay(10);
+  //
+  //Configuração do ESC
+  //
 
-//
-//Conectar com Wifi
-//
+  weaponMotor.attach(15, 1000, 2000);  // attaches the servo on GIO2 to the servo object  
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  // Iniciando ESC
+  //  delay(5000);
+  Serial.println("Iniciando Configuração da ESC");
+  Serial.println("Mapeando Max");
+ 
+  for (int i = 0; i < 40; i++)  {
+    weaponMotor.writeMicroseconds(2000);
+    delay(50);
+  } 
+ 
+  digitalWrite(LED_BUILTIN, LOW); 
+  
+  Serial.println("Mapeando Min");
+  for (int i = 0; i < 40; i++) {
+    weaponMotor.writeMicroseconds(1000);
+    delay(50);
+  }
+  Serial.println("Fim de configuração da ESC");
+  //
+  //Conectar com Wifi
+  //
   Serial.println("");
   Serial.println("");
   Serial.print("conenctando a ");
@@ -66,38 +94,12 @@ void setup() {
     Serial.print(".");
   }
 
-  udp.begin(80);
+  udp.begin(80);  
+}
 
-//
-//Configuração do ESC
-//
-
-  myservo.attach(15, 1000, 2000);  // attaches the servo on GIO2 to the servo object  
-
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  // Iniciando ESC
-//  delay(5000);
-  Serial.println("Iniciando Configuração");
-  Serial.println("Max");
- 
-  for (int i = 0; i < 40; i++)  {
-    myservo.writeMicroseconds(2000);
-    delay(50);
-  } 
- 
-  digitalWrite(LED_BUILTIN, LOW); 
-//  while(Serial.available() <=0);
-  
-  Serial.println("Min");
-  for (int i = 0; i < 40; i++) {
-    myservo.writeMicroseconds(1000);
-    delay(50);
-  }
-  Serial.println("Fim de configuração");
-  
-  
+void sendToWeapon(int power) {
+  pow_esc = 1000 + power * 10;
+  weaponMotor.writeMicroseconds(pow_esc);
 }
 
 void sendToMotor(int powerML, int powerMR){
@@ -119,9 +121,8 @@ void sendToMotor(int powerML, int powerMR){
   powerML = powerML * (1023 / 100.0);
   powerMR = powerMR * (1023 / 100.0);
 
-  analogWrite(MLPWM, powerML);
-  analogWrite(MRPWM, powerMR);
-  
+  analogWrite(MLPWM, abs(powerML));
+  analogWrite(MRPWM, abs(powerMR));
 }
 
 void stopA(){
@@ -150,28 +151,51 @@ void loop() {
         char z = udp.read();
         request+= z;
       }
+
+      last_message_time = millis();
       /*
-      * -100+1001
-      * ---------
-      * 000000000
-      * 012345678
+      * -100+1001666
+      * ------------
+      * 000000000011
+      * 012345678901
+      * 
+      * [0, 3]: velocidade linear
+      * [4, 7]: velocidade angular
+      * [8]: switch da arma
+      * [9, 11]: potencia da arma
       */
       pow_linear = request.substring(0, 4).toInt(); // [-100:100]
       pow_angular = request.substring(4, 8).toInt(); // [-50:50]
-      weapon = request.substring(8).toInt(); // [0:1]
-
-
-      Serial.println("------------");
-      Serial.println(pow_linear);
-      Serial.println(pow_angular);
-      Serial.println(weapon);
-      Serial.println("------------");
-      
-      weaponMotor.writeMicroseconds(weapon * 1700);
-    
-    weaponMotor.writeMicroseconds(weapon * 1700);
-
-//    sendToMotor(MLPWM, pow_linear);
-//    sendToMotor(MRPWM, pow_linear);
+      weapon_status = request.substring(8, 9).toInt(); // [0:1]
+      weapon_power = request.substring(9).toInt(); // [0:100]
+  } else {
+      // IDLE MODE (NO COMM)
+      if ((millis() - last_message_time) > MESSAGE_TIMEOUT) {
+        pow_linear = 0; // [-100:100]
+        pow_angular = 0; // [-100:100]
+        weapon_status = 0; // [0:1]
+        weapon_power = 0; // [0:100]
+      }
   }
+  Serial.println("------------");
+  Serial.println(pow_linear);
+  Serial.println(pow_angular);
+  Serial.println(weapon_status);
+  Serial.println(weapon_power);
+  Serial.println("------------");
+
+  if(weapon_status) {
+    sendToWeapon(weapon_power);
+  } else {
+    sendToWeapon(0);
+  }
+  
+  if (pow_linear != 0) {
+    pow_angular/2;
+  }
+  
+  pow_left = pow_linear + pow_angular;
+  pow_right = pow_linear - pow_angular;
+  
+  sendToMotor(pow_left, pow_right);
 }
